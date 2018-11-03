825a706c98d0b90b4e425747d2a6cfcba2aca962
Chethan K S <chethan1729@gmail.com>
2018-11-03 11:34:03 +0530

    [2] Added state property to burger ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
 const burger = (props) => {
+    
+    const transaformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />; 
+        }); 
+    });
+
     return (
        <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transaformedIngredients}
             <BurgerIngredient type="bread-bottom" />       
        </div> 
     );
