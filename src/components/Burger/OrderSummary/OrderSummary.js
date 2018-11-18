913869791135563849e833b1193924102709b97c
Chethan K S <chethan1729@gmail.com>
2018-11-18 17:07:23 +0530

    [5] Added sidedrawer for mobile view

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,44 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxi';
+import React,{Component} from 'react';
+import Aux from '../../../hoc/Auxi/Auxi';
 import Button from '../../UI/Button/Button';
 import classes from './OrderSummary.css';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
 
-    const ingredientSummary = Object.keys(props.ingredients)
+    ComponentWillUpdate (){
+        console.log('[OrderSummary] WillUpdate');
+    }
+
+    render() {
+
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>:{props.ingredients[igKey]}
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>:{this.props.ingredients[igKey]}
                 </li>
             );
         });
-    return (
-        <Aux>
-            <div className={classes.OrderFont}>
-                <h3> Please review your order :</h3>
-            </div>
-            <p> A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price : {props.price} Rs</strong></p>
-            <p> Continue to Checkout</p>
-            <div align='center'>
-                <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-                <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-            </div>
-        </Aux>
-    );
-};
+        
+        return (
+            <Aux>
+                <div className={classes.OrderFont}>
+                    <h3> Please review your order :</h3>
+                </div>
+                <p> A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price : {this.props.price} Rs</strong></p>
+                <p> Continue to Checkout</p>
+                <div align='center'>
+                    <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                    <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+                </div>
+            </Aux>
+        );
+    }
 
-export default orderSummary;
+}
+
+export default OrderSummary;
